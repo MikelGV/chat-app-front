727e54c2f7a243bfca9c2c5f121cbe2b0562b1f9
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-21 01:13:28 +0200

    things changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useSockets } from '../context/socket.context'
-import styles from '../styles/Home.module.css'
-import RoomsContainer from '../containers/rooms'
-import MessagesContainer from '../containers/messages'
-import { useEffect, useRef } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useSockets } from "../context/socket.context";
+
+import RoomsContainer from "../containers/rooms";
+import MessagesContainer from "../containers/messages";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-
   const { socket, username, setUsername } = useSockets();
   const usernameRef = useRef(null);
-  
+
   function handleSetUsername() {
     const value = usernameRef.current.value;
     if (!value) {
@@ -32,8 +32,10 @@ export default function Home() {
       {!username && (
         <div className={styles.usernameWrapper}>
           <div className={styles.usernameInner}>
-            <input placeholder="Username" ref={usernameRef} />  
-            <button className="cta" onClick={handleSetUsername}>Start</button>
+            <input placeholder="Username" ref={usernameRef} />
+            <button className="cta" onClick={handleSetUsername}>
+              START
+            </button>
           </div>
         </div>
       )}
@@ -44,5 +46,5 @@ export default function Home() {
         </div>
       )}
     </div>
-  )
+  );
 }
